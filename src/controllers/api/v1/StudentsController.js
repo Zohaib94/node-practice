b06eaf3d307b3d4751006e8bfc81628f6b686e41
Zohaib Ahmed Butt <zohaibahmedbutt@gmail.com>
2020-08-30 23:41:08 +0300

    Student: Updated Controller

--- src/controllers/api/v1/StudentsController.js
+++ src/controllers/api/v1/StudentsController.js
@@ -1,12 +1,28 @@
 import { Router } from 'express';
-import Student from '../../../models/student';
+import StudentService from '../../../services/StudentService';
+import SuccessResponse from '../../../responses/SuccessResponse';
 
 // Router is isolated instance of middleware and routes capable of only routing and middleware functions
 const StudentsController = Router();
 
 StudentsController.get('/', async (request, response) => {
-  const students = await Student.findAll();
-  response.json(students);
+  try {
+    const students = await StudentService.findAll();
+    response.json(new SuccessResponse(students));
+  } catch (err) {
+    console.log(err);
+    response.status(err.code).json(err.getResource());
+  }
+});
+
+StudentsController.post('/', async (request, response) => {
+  try {
+    const student = await StudentService.createStudent(request.body);
+    response.json(new SuccessResponse(student));
+  } catch (err) {
+    console.log(err);
+    response.status(err.code).json(err.getResource());
+  }
 });
 
 export default StudentsController;
