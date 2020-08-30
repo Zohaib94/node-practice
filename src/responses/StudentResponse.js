d0d9c9286b5c75510ca4087c13280c9b9ea4451b
Zohaib Ahmed Butt <zohaibahmedbutt@gmail.com>
2020-08-30 23:40:02 +0300

    Student: Added Response Objects

---
+++ src/responses/StudentResponse.js
@@ -0,0 +1,9 @@
+class StudentResponse {
+  constructor(id = '', firstName = '', lastName = '') {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.id = id;
+  }
+}
+
+export default StudentResponse;
