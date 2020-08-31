a69dbbcab95080efde3be034c705334ecbff2e81
Zohaib Ahmed Butt <zohaibahmedbutt@gmail.com>
2020-08-31 14:50:17 +0300

    Student: Added service method to add ISIC Card

--- src/services/StudentService.js
+++ src/services/StudentService.js
@@ -1,5 +1,6 @@
 import Student from '../models/Student';
 import StudentSerializer from '../serializers/StudentSerializer';
+import ISICCardSerializer from '../serializers/ISICCardSerializer';
 import ErrorResponse from '../responses/ErrorResponse';
 
 class StudentService {
@@ -18,6 +19,26 @@ class StudentService {
       const newStudent = await Student.create(params);
 
       return StudentSerializer.toResource(newStudent);
+    } catch (error) {
+      throw new ErrorResponse(error.message, 422);
+    }
+  }
+
+  static async createISICCard(studentID, params) {
+    try {
+      const student = await Student.findByPk(studentID);
+
+      if (!student) {
+        throw new Error('Student Not Found');
+      }
+
+      if (student.getISICCard()) {
+        throw new Error('Student already has an ISIC Card');
+      }
+
+      const isicCard = await student.createISICCard(params);
+
+      return ISICCardSerializer.toResource(isicCard);
     } catch (error) {
       throw new ErrorResponse(error.message, 422);
     }
