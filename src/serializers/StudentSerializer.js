18d9cd9852a477798e4f385a706b917ef0f12f06
Zohaib Ahmed Butt <zohaibahmedbutt@gmail.com>
2020-08-31 14:51:18 +0300

    Student: Fixed bug in serializer

--- src/serializers/StudentSerializer.js
+++ src/serializers/StudentSerializer.js
@@ -3,9 +3,9 @@ import StudentResponse from '../responses/StudentResponse';
 class StudentSerializer {
   static toResource(studentObject) {
     const studentResource = new StudentResponse(
+      studentObject.id,
       studentObject.firstName,
       studentObject.lastName,
-      studentObject.id,
     );
     return studentResource;
   }
